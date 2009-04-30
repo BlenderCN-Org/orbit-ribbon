f59250246e6eb0a991ecdac8baa02750e2da07b4
DavidMikeSimon <david.mike.simon@gmail.com>
2009-04-30 01:10:31 +0000

    Improved rendering of the far side of the smoke ring a little bit

--- sky.py
+++ sky.py
@@ -9,13 +9,13 @@ from util import *
 # Distances in meters from Voy to other sky objects, and sizes of various objects
 VOY_RADIUS = 2e4 # From book
 T3_DIST = 2.5e11 # From book
-T3_RADIUS = 8.4e8 # Our Sun's radius times about 1.2, which is from book
+T3_RADIUS = 2.5e9 # Our Sun's radius times about 1.2 is 8.4e8, which is from book... but that looks too small
 TORUS_OUTSIDE_DIST = 1e9 # From book
 TORUS_INSIDE_DIST = 1e7 # Guessed
 TORUS_RADIUS = TORUS_OUTSIDE_DIST - TORUS_INSIDE_DIST # Assuming a circular torus, which probably isn't quite right
 GOLD_DIST = 2.6e7 # From book; also, assuming that Gold is in precise middle of Smoke Ring
-GOLD_RADIUS = 5e6 # Guessed; includes the storm around Gold
-SMOKE_RING_RADIUS = 1.4e7 # Calculated; book says that SR has volume of 1e14 cubic km, and assumed a circular torus...
+GOLD_RADIUS = 5e5 # Guessed; includes the storm around Gold
+SMOKE_RING_RADIUS = 8e4 # Calculations yielded 1.4e7, but that looked terrible, so this is made up 
 SMOKE_RING_INSIDE_DIST = GOLD_DIST - SMOKE_RING_RADIUS
 SMOKE_RING_OUTSIDE_DIST = GOLD_DIST + SMOKE_RING_RADIUS
 
@@ -44,23 +44,34 @@ class SkyStuff(gameobj.GameObj):
 		self._t3_tex = resman.Texture("t3.png")
 		self._gold_tex = resman.Texture("gold.png")
 	
-	def indraw(self):	
-		# The Smoke Ring
-		# FIXME - Faked out values to achieve thin-line look
-		glColor4f(1.0, 1.0, 1.0, 0.3)
+	def indraw(self):
+		
+		# The Smoke Ring and the gas torus
+		glEnable(GL_CULL_FACE)
 		glPushMatrix()
-		glTranslatef(0, 0, GOLD_DIST*299)
 		glRotatef(90, 1, 0, 0)
-		glutSolidTorus(SMOKE_RING_RADIUS, GOLD_DIST*300, 100, 20)
+		# Smoke ring itself
+		glColor4f(1.0, 1.0, 1.0, 0.5)
+		glutSolidTorus(SMOKE_RING_RADIUS, GOLD_DIST, 20, 20)
+		# Outer "fuzz" from the smoke ring
+		for i in range(3, 20, 3):
+			glRotatef(2, 0, 0, 1)
+			glColor4f(1.0, 1.0, 1.0, 0.08)
+			glutSolidTorus(SMOKE_RING_RADIUS*i, GOLD_DIST, 20, 20)
+		# The gas torus
+		# FIXME - Rendering it effectively only tints background color. Should it do something else? Should I bother?
+		#glColor4f(1.0, 1.0, 1.0, 0.05)
+		#glutSolidTorus(TORUS_RADIUS, (TORUS_INSIDE_DIST+TORUS_OUTSIDE_DIST)/2, 20, 20)
 		glPopMatrix()
-
+		glDisable(GL_CULL_FACE)
+		
 		# FIXME - Verify that this makes T3 spin in the correct direction
 		t3_pos = Point(math.sin(rev2rad(self.day_elapsed))*T3_DIST, 0, math.cos(rev2rad(self.day_elapsed))*T3_DIST)
 		
 		glEnable(GL_TEXTURE_2D)
 		glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE)
-
-		# Set up lighting parameters for the two stars
+		
+		# Set up lighting parameters for the two stars of the Smoke Ring system
 		# Since lighting is disabled at this time, this does not affect the drawing of the sky objects themselves
 		
 		# T3
@@ -71,7 +82,7 @@ class SkyStuff(gameobj.GameObj):
 		glLightfv(GL_LIGHT1, GL_AMBIENT, (0.7, 0.7, 0.7, 1.0))
 		glLightfv(GL_LIGHT1, GL_DIFFUSE, (1.0, 1.0, 1.0, 1.0))
 		glEnable(GL_LIGHT1)
-
+		
 		# Voy
 		# FIXME - Set up lighting for Voy
 		
