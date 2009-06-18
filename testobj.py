3ba7bf10eff06822c1cab8209e8a78f10271db5d
DavidMikeSimon <david.mike.simon@gmail.com>
2009-06-18 23:29:03 +0000

    Added thickness to the test ground object

--- testobj.py
+++ testobj.py
@@ -26,10 +26,10 @@ class Cube(gameobj.GameObj):
 
 
 class Ground(gameobj.GameObj):
-	"""A non-movable planar ground object for testing."""
-
+	"""A non-movable rectangular ground object for testing."""
+	
 	def __init__(self, pos, scale = 100.0):
-		geom = ode.GeomBox(app.static_space, (scale, 0.01, scale))
+		geom = ode.GeomBox(app.static_space, (scale, 1.0, scale))
 		geom.coll_props = collision.Props()
 		super(Ground, self).__init__(pos = pos, body = None, geom = geom)
 		self._scale = scale
@@ -37,21 +37,40 @@ class Ground(gameobj.GameObj):
 	
 	def indraw(self):
 		scale = self._scale
-		tscale = 4 # Scaling of the texture
+		tscale = 0.25 # Scaling of the texture
 		glEnable(GL_TEXTURE_2D)
 		glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE)
 		glBindTexture(GL_TEXTURE_2D, self._tex.glname)
 		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
 		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+		y = 0.5
+		tul = Point(-scale/2,  y, -scale/2)
+		tll = Point(-scale/2,  y,  scale/2)
+		tlr = Point( scale/2,  y,  scale/2)
+		tur = Point( scale/2,  y, -scale/2)
+		bul = Point(-scale/2, -y, -scale/2)
+		bll = Point(-scale/2, -y,  scale/2)
+		blr = Point( scale/2, -y,  scale/2)
+		bur = Point( scale/2, -y, -scale/2)
 		glBegin(GL_QUADS)
-		glTexCoord2f(0.0, 0.0)
-		glVertex3f(-scale/2, 0, -scale/2)
-		glTexCoord2f(0.0, scale/tscale)
-		glVertex3f(-scale/2, 0, scale/2)
-		glTexCoord2f(scale/tscale, scale/tscale)
-		glVertex3f(scale/2, 0, scale/2)
-		glTexCoord2f(scale/tscale, 0.0)
-		glVertex3f(scale/2, 0, -scale/2)
+		for a, b, c, d in (
+			(tul, tll, tlr, tur),
+			(bul, bll, blr, bur),
+			(tul, bul, bur, tur),
+			(tll, bll, blr, tlr),
+			(tul, bul, bll, tll),
+			(tur, bur, blr, tlr),
+		):
+			ab_dist = a.dist_to(b)
+			bc_dist = b.dist_to(c)
+			glTexCoord2f(0.0, 0.0)
+			glVertex3f(*a)
+			glTexCoord2f(0.0, ab_dist*tscale)
+			glVertex3f(*b)
+			glTexCoord2f(bc_dist*tscale, ab_dist*tscale)
+			glVertex3f(*c)
+			glTexCoord2f(bc_dist*tscale, 0.0)
+			glVertex3f(*d)
 		glEnd()
 		glDisable(GL_TEXTURE_2D)
 
