baaf454a046d3fef7d104ac7900a70ad71f4c539
DavidMikeSimon <david.mike.simon@gmail.com>
2009-06-28 21:34:22 +0000

    Continuing to screw around with Blender scripting. It still isn't doing anything productive.

--- blender/orbit-edit.py
+++ blender/orbit-edit.py
@@ -1,15 +1,7 @@
-import Blender
-from Blender import NMesh
-from Blender.BGL import *
-from Blender.Draw import *
-
-import os
+import Blender, os
 from math import *
 
 WORKING_DIR = os.path.dirname(Blender.Get("filename"))
-
-# Events
-EVENT_NOEVENT, EVENT_ADD_GO, EVENT_EXPORT, EVENT_QUIT = range(4)
 
 def list_gameobj_blends():
 	"""Returns a list of blend files which (appear to) contain game objects."""
@@ -18,6 +10,29 @@ def list_gameobj_blends():
 		if fn.startswith("go-") and fn.endswith(".blend"):
 			ret.append(fn)
 	return ret
+
+
+def do_export():
+	print "Exporting..."
+	Blender.Draw.PupMenu("Exported Just Fine%t|OK")
+
+
+def do_add_gameobj(blend_fn):
+	print "Adding Game Object from %s" % blend_fn
+	short_name = blend_fn[3:-6] # Extract the "go-" and the ".blend" from the filename
+	
+	# Find the lowest-numbered object we can get
+	obj_name_list = [x.name for x in Blender.Object.Get()]
+	n = 1
+	while 1:
+		obj_name = "GO-%s%04u" % (short_name, n)
+		if obj_name not in obj_name_list:
+			break
+	obj = Blender.Object.New("Mesh") # Adding a new Mesh Object (as distinct from a new Mesh)
+	
+	Blender.Library.Open(os.path.join(WORKING_DIR, blend_fn))
+	mesh = Blender.Library.Load("GameObj", "Mesh")
+	Blender.Library.Close()
 
 
 def polygon(NumberOfSides,Radius):
@@ -52,44 +67,17 @@ def polygon(NumberOfSides,Radius):
 	Blender.Redraw()
 
 
-def gui_draw():
-	X_MARGIN = 10
-	Y_STEP = 23
-	HEIGHT = 18
-	WIDTH = 150
-	
-	glClear(GL_COLOR_BUFFER_BIT)
-	
-	# Due to OpenGL conventions, we draw from the bottom to the top
-	y = Y_STEP - HEIGHT
-	Button("Export", EVENT_EXPORT, X_MARGIN, y, WIDTH, HEIGHT)
-	y += Y_STEP
-	Button("Add GameObject", EVENT_ADD_GO, X_MARGIN, y, WIDTH, HEIGHT)
-	y += Y_STEP
-	Label("Orbit Ribbon Level Editor", X_MARGIN, y, WIDTH, HEIGHT)
+def menu():
+	menu = ("Add GameObject", "Export")
+	r = Blender.Draw.PupMenu("Orbit Ribbon Level Editing%t|" + "|".join(["%s%%x%u" % (name, num) for num, name in enumerate(menu)]))
+	if r >= 0:
+		if menu[r] == "Add GameObject":
+			gameobj_blends = list_gameobj_blends()
+			sub_r = Blender.Draw.PupMenu("Select GameObject%t|" + "|".join(["%s%%x%u" % (name, num) for num, name in enumerate(gameobj_blends)]))
+			if sub_r >= 0:
+				do_add_gameobj(gameobj_blends[sub_r])
+		elif menu[r] == "Export":
+			do_export()
+	Blender.Redraw()
 
-
-def gui_event(evt, val):	
-	if evt == QKEY and not val: 
-		Exit()
-
-
-def gui_bevent(evt):
-	if evt == EVENT_EXPORT:
-		do_export()
-	elif evt == EVENT_ADD_GO:
-		gameobj_blends = list_gameobj_blends()
-		r = Blender.Draw.PupMenu("Select GameObject%t|" + "|".join(["%s%%x%u" % (name, num) for num, name in enumerate(gameobj_blends)]))
-		if r >= 0:
-			do_add_gameobj(gameobj_blends[r])
-	if evt != EVENT_NOEVENT:
-		Blender.Redraw()
-
-
-Register(gui_draw, gui_event, gui_bevent)
-
-def do_export():
-	print "Exporting..."
-
-def do_add_gameobj(blend_fn):
-	print "Adding Game Object from %s" % blend_fn
+menu()
