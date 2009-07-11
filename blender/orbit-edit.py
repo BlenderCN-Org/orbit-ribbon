79dc8f1c065632a569cd6cf03849e8ad42964d4e
DavidMikeSimon <david.mike.simon@gmail.com>
2009-07-11 00:53:36 +0000

    Working on correcting y-mirroring issue. Have it fixed in position, but attempts to change the rotation matrix caused only unwanted chaos and weird behavior in collision engine.

--- blender/orbit-edit.py
+++ blender/orbit-edit.py
@@ -9,13 +9,17 @@ sys.path.append(os.path.join(WORKING_DIR, os.path.pardir))
 import editorexport
 
 def fixcoords(t): # Given a 3-tuple, returns it so that axes changed to fit OpenGL standards (i.e. y is up, z is out)
-	return (t[0], t[2], t[1])
+	return (t[0], t[2], -t[1])
 
 def fixrotmatrix(m): # Given a 4x4 row-major transform matrix, returns a 9-tuple for a column-major 3x3 rotation matrix with axes corrected ala fixcoords
 	return (
+	#	 m[0][0],  m[2][0], -m[1][0],
+	# 	 m[0][2],  m[2][2], -m[1][2],
+	#	-m[0][1], -m[2][1], -m[1][1],
 		m[0][0], m[0][2], m[0][1],
 		m[2][0], m[2][2], m[2][1],
 		m[1][0], m[1][2], m[1][1],
+
 	)
 
 def pup_error(msg):
