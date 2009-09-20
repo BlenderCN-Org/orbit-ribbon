1ed685cc059ef25c7e35a5c16e845615bba77cb2
DavidMikeSimon <david.mike.simon@gmail.com>
2009-09-20 23:48:05 +0000

    Have an acceptable base OpenGL timing test

--- shuttle/basetest/test.c
+++ shuttle/basetest/test.c
@@ -20,7 +20,7 @@
 /* screen width, height, and bit depth */
 #define SCREEN_WIDTH  800
 #define SCREEN_HEIGHT 600
-#define SCREEN_BPP     16
+#define SCREEN_BPP	 16
 
 /* Define our booleans */
 #define TRUE  1
@@ -30,227 +30,227 @@
 SDL_Surface *surface;
 
 /* function to release/destroy our resources and restoring the old desktop */
-void Quit( int returnCode )
-{
-    /* clean up the window */
-    SDL_Quit( );
+void Quit( int returnCode ) {
+	/* clean up the window */
+	SDL_Quit();
 
-    /* and exit appropriately */
-    exit( returnCode );
+	/* and exit appropriately */
+	exit( returnCode );
 }
 
 /* function to reset our viewport after a window resize */
-int resizeWindow( int width, int height )
-{
-    /* Height / width ration */
-    GLfloat ratio;
-
-    /* Protect against a divide by zero */
-    if ( height == 0 )
+int resizeWindow( int width, int height ) {
+	/* Height / width ration */
+	GLfloat ratio;
+	
+	/* Protect against a divide by zero */
+	if ( height == 0 )
 	height = 1;
-
-    ratio = ( GLfloat )width / ( GLfloat )height;
-
-    /* Setup our viewport. */
-    glViewport( 0, 0, ( GLsizei )width, ( GLsizei )height );
-
-    /* change to the projection matrix and set our viewing volume. */
-    glMatrixMode( GL_PROJECTION );
-    glLoadIdentity( );
-
-    /* Set our perspective */
-    gluPerspective( 45.0f, ratio, 0.1f, 100.0f );
-
-    /* Make sure we're chaning the model view and not the projection */
-    glMatrixMode( GL_MODELVIEW );
-
-    /* Reset The View */
-    glLoadIdentity( );
-
-    return( TRUE );
+	
+	ratio = ( GLfloat )width / ( GLfloat )height;
+	
+	/* Setup our viewport. */
+	glViewport( 0, 0, ( GLsizei )width, ( GLsizei )height );
+	
+	/* change to the projection matrix and set our viewing volume. */
+	glMatrixMode( GL_PROJECTION );
+	glLoadIdentity();
+	
+	/* Set our perspective */
+	gluPerspective( 45.0f, ratio, 0.1f, 100.0f );
+	
+	/* Make sure we're chaning the model view and not the projection */
+	glMatrixMode( GL_MODELVIEW );
+	
+	/* Reset The View */
+	glLoadIdentity();
+	
+	return( TRUE );
 }
 
 /* general OpenGL initialization function */
-int initGL( GLvoid )
-{
+int initGL( GLvoid ) {
 
-    /* Enable smooth shading */
-    glShadeModel( GL_SMOOTH );
+	/* Enable smooth shading */
+	glShadeModel( GL_SMOOTH );
 
-    /* Set the background black */
-    glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+	/* Set the background black */
+	glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
 
-    /* Depth buffer setup */
-    glClearDepth( 1.0f );
+	/* Depth buffer setup */
+	glClearDepth( 1.0f );
 
-    /* Enables Depth Testing */
-    glEnable( GL_DEPTH_TEST );
+	/* Enables Depth Testing */
+	glEnable( GL_DEPTH_TEST );
 
-    /* The Type Of Depth Test To Do */
-    glDepthFunc( GL_LEQUAL );
+	/* The Type Of Depth Test To Do */
+	glDepthFunc( GL_LEQUAL );
 
-    /* Really Nice Perspective Calculations */
-    glHint( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST );
+	/* Really Nice Perspective Calculations */
+	glHint( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST );
 
-    return( TRUE );
+	return( TRUE );
 }
 
 /* Here goes our drawing code */
-int drawGLScene( GLvoid )
-{
-    /* rotational vars for the triangle and quad, respectively */
-    static GLfloat rtri, rquad;
+int drawGLScene( GLvoid ) {
+	/* rotational vars for the triangle and quad, respectively */
+	static GLfloat rtri, rquad;
+
+	GLint t = SDL_GetTicks();
 	
-    /* Clear The Screen And The Depth Buffer */
-    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+	/* Clear The Screen And The Depth Buffer */
+	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 	
-	 glPushMatrix();
-	 glTranslatef(0.0f, 0.0f, -6.0f);
+	glPushMatrix();
+	glTranslatef(0.0f, 0.0f, -6.0f);
+	glScalef(0.05, 0.05, 0.05);
+	
+	int row, col;
+	for (row = 0; row < 30; ++row) {
+		for (col = 0; col < 40; ++col) {
+			glPushMatrix();
+			glTranslatef(-60 + col*3, 45 - row*3, 0);
+			//glTranslatef(-100 + row, -100 + col, 0);
+			
+			glPushMatrix();
+			glRotatef( rtri, 0.0f, 1.0f, 0.0f );
+			glBegin( GL_TRIANGLES );			 /* Drawing Using Triangles	   */
+				glColor3f(   1.0f,  0.0f,  0.0f ); /* Red						   */
+				glVertex3f(  0.0f,  1.0f,  0.0f ); /* Top Of Triangle			   */
+				glColor3f(   0.0f,  1.0f,  0.0f ); /* Green						 */
+				glVertex3f( -1.0f, -1.0f,  0.0f ); /* Left Of Triangle			  */
+				glColor3f(   0.0f,  0.0f,  1.0f ); /* Blue						  */
+				glVertex3f(  1.0f, -1.0f,  0.0f ); /* Right Of Triangle			 */
+			glEnd();							/* Finished Drawing The Triangle */
+			glPopMatrix();
 
-	 glPushMatrix();
-    glRotatef( rtri, 0.0f, 1.0f, 0.0f );
-    glBegin( GL_TRIANGLES );             /* Drawing Using Triangles       */
-      glColor3f(   1.0f,  0.0f,  0.0f ); /* Red                           */
-      glVertex3f(  0.0f,  1.0f,  0.0f ); /* Top Of Triangle               */
-      glColor3f(   0.0f,  1.0f,  0.0f ); /* Green                         */
-      glVertex3f( -1.0f, -1.0f,  0.0f ); /* Left Of Triangle              */
-      glColor3f(   0.0f,  0.0f,  1.0f ); /* Blue                          */
-      glVertex3f(  1.0f, -1.0f,  0.0f ); /* Right Of Triangle             */
-    glEnd( );                            /* Finished Drawing The Triangle */
-	 glPopMatrix();
+			glPushMatrix();
+			glTranslatef(0.0f, 0.0f, -3.0f);
+			glRotatef( rquad, 1.0f, 0.0f, 0.0f );
+			glColor3f( 0.5f, 0.5f, 1.0f);
+			glBegin( GL_QUADS );				 /* Draw A Quad			  */
+				glVertex3f(  1.0f,  1.0f,  0.0f ); /* Top Right Of The Quad	*/
+				glVertex3f( -1.0f,  1.0f,  0.0f ); /* Top Left Of The Quad	 */
+				glVertex3f( -1.0f, -1.0f,  0.0f ); /* Bottom Left Of The Quad  */
+				glVertex3f(  1.0f, -1.0f,  0.0f ); /* Bottom Right Of The Quad */
+			glEnd();							/* Done Drawing The Quad	*/
+			glPopMatrix();
 
-    glPushMatrix();
-    glRotatef( rquad, 1.0f, 0.0f, 0.0f );
-    glColor3f( 0.5f, 0.5f, 1.0f);
-    glBegin( GL_QUADS );                 /* Draw A Quad              */
-      glVertex3f(  1.0f,  1.0f,  0.0f ); /* Top Right Of The Quad    */
-      glVertex3f( -1.0f,  1.0f,  0.0f ); /* Top Left Of The Quad     */
-      glVertex3f( -1.0f, -1.0f,  0.0f ); /* Bottom Left Of The Quad  */
-      glVertex3f(  1.0f, -1.0f,  0.0f ); /* Bottom Right Of The Quad */
-    glEnd( );                            /* Done Drawing The Quad    */
-	 glPopMatrix();
-
-	 glPopMatrix();
-
-    /* Draw it to the screen */
-    SDL_GL_SwapBuffers( );
-
-
-    /* Increase The Rotation Variable For The Triangle ( NEW ) */
-    rtri  += 0.2f;
-    /* Decrease The Rotation Variable For The Quad     ( NEW ) */
-    rquad -=0.15f;
-
-    return( TRUE );
+			glPopMatrix();
+		}
+	}
+	
+	glPopMatrix();
+	
+	/* Draw it to the screen */
+	SDL_GL_SwapBuffers();
+	
+	GLint tdiff = SDL_GetTicks() - t;
+	
+	/* Increase The Rotation Variable For The Triangle ( NEW ) */
+	rtri  += 0.2f*tdiff;
+	/* Decrease The Rotation Variable For The Quad	 ( NEW ) */
+	rquad -=0.15f*tdiff;
+	
+	return( TRUE );
 }
 
-int main( int argc, char **argv )
-{
-    /* Flags to pass to SDL_SetVideoMode */
-    int videoFlags;
-    /* main loop variable */
-    int done = FALSE;
-    /* used to collect events */
-    SDL_Event event;
-    /* this holds some info about our display */
-    const SDL_VideoInfo *videoInfo;
+int main( int argc, char **argv ) {
+	/* Flags to pass to SDL_SetVideoMode */
+	int videoFlags;
+	/* main loop variable */
+	int done = FALSE;
+	/* used to collect events */
+	SDL_Event event;
+	/* this holds some info about our display */
+	const SDL_VideoInfo *videoInfo;
 
-    /* initialize SDL */
-    if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-	    fprintf( stderr, "Video initialization failed: %s\n",
-		     SDL_GetError( ) );
-	    Quit( 1 );
+	/* initialize SDL */
+	if ( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
+	   fprintf( stderr, "Video initialization failed: %s\n", SDL_GetError() );
+	   Quit( 1 );
 	}
 
-    /* Fetch the video info */
-     videoInfo = SDL_GetVideoInfo( );
+	/* Fetch the video info */
+	 videoInfo = SDL_GetVideoInfo();
 
-     if ( !videoInfo )
-	{
-	    fprintf( stderr, "Video query failed: %s\n",
-		     SDL_GetError( ) );
-	    Quit( 1 );
+	 if ( !videoInfo ) {
+	   fprintf( stderr, "Video query failed: %s\n", SDL_GetError() );
+	   Quit( 1 );
 	}
 
-    /* the flags to pass to SDL_SetVideoMode */
-    videoFlags  = SDL_OPENGL;          /* Enable OpenGL in SDL */
-    videoFlags |= SDL_GL_DOUBLEBUFFER; /* Enable double buffering */
-    videoFlags |= SDL_HWPALETTE;       /* Store the palette in hardware */
+	/* the flags to pass to SDL_SetVideoMode */
+	videoFlags  = SDL_OPENGL;		  /* Enable OpenGL in SDL */
+	videoFlags |= SDL_GL_DOUBLEBUFFER; /* Enable double buffering */
+	videoFlags |= SDL_HWPALETTE;	   /* Store the palette in hardware */
 
-    /* This checks to see if surfaces can be stored in memory */
-    if ( videoInfo->hw_available )
-	videoFlags |= SDL_HWSURFACE;
-    else
-	videoFlags |= SDL_SWSURFACE;
+	/* This checks to see if surfaces can be stored in memory */
+	if ( videoInfo->hw_available ) {
+		videoFlags |= SDL_HWSURFACE;
+	} else {
+		videoFlags |= SDL_SWSURFACE;
+	}
 
-    /* This checks if hardware blits can be done */
-    if ( videoInfo->blit_hw )
-	videoFlags |= SDL_HWACCEL;
+	/* This checks if hardware blits can be done */
+	if ( videoInfo->blit_hw ) videoFlags |= SDL_HWACCEL;
 
-    /* Sets up OpenGL double buffering */
-    SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
+	/* Sets up OpenGL double buffering */
+	SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
 
-    /* get a SDL surface */
-    surface = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP,
+	/* get a SDL surface */
+	surface = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP,
 				videoFlags );
 
-    /* Verify there is a surface */
-    if ( !surface )
-	{
-	    fprintf( stderr,  "Video mode set failed: %s\n", SDL_GetError( ) );
-	    Quit( 1 );
+	/* Verify there is a surface */
+	if ( !surface ) {
+		fprintf( stderr,  "Video mode set failed: %s\n", SDL_GetError() );
+		Quit( 1 );
 	}
 
-    /* initialize OpenGL */
-    initGL( );
+	/* initialize OpenGL */
+	initGL();
 
-    /* resize the initial window */
-    resizeWindow( SCREEN_WIDTH, SCREEN_HEIGHT );
-    
-	 /* These are to calculate our fps */
-    GLint frames = 0;
-    GLint t0     = 0;
-	 t0 = SDL_GetTicks();
+	/* resize the initial window */
+	resizeWindow( SCREEN_WIDTH, SCREEN_HEIGHT );
+	
+	/* These are to calculate our fps */
+	GLint frames = 0;
+	GLint t0	 = 0;
+	t0 = SDL_GetTicks();
 
-    /* wait for events */ 
-    while ( !done )
-	{
-	    /* handle the events in the queue */
+	/* wait for events */ 
+	while ( !done ) {
+	   /* handle the events in the queue */
 
-	    while ( SDL_PollEvent( &event ) )
-		{
-		    switch( event.type )
-			{
-			case SDL_QUIT:
-			    /* handle quit requests */
-			    done = TRUE;
-			    break;
-			default:
-			    break;
+	   while ( SDL_PollEvent( &event ) ) {
+		   switch( event.type ) {
+				case SDL_QUIT:
+					/* handle quit requests */
+					done = TRUE;
+					break;
+				default:
+					break;
 			}
 		}
 
-	    /* draw the scene */
-		drawGLScene( );
-    
-		 /* Gather our frames per second */
-		 ++frames;
-		 {
+	   /* draw the scene */
+		drawGLScene();
+	
+		/* Gather our frames per second */
+		++frames;
 		GLint t = SDL_GetTicks();
-		if (t - t0 >= 5000) {
-			 GLfloat seconds = (t - t0) / 1000.0;
-			 GLfloat fps = frames / seconds;
-			 printf("%d frames in %g seconds = %g FPS\n", frames, seconds, fps);
-			 t0 = t;
-			 frames = 0;
+		if (t - t0 >= 10000) {
+			GLfloat seconds = (t - t0) / 1000.0;
+			GLfloat fps = frames / seconds;
+			printf("%d frames in %g seconds = %g FPS\n", frames, seconds, fps);
+			done = TRUE;
 		}
-		 }
 	}
 
-    /* clean ourselves up and exit */
-    Quit( 0 );
+	/* clean ourselves up and exit */
+	Quit(0);
 
-    /* Should never get here */
-    return( 0 );
+	/* Should never get here */
+	return(0);
 }
