c598680d06bd96f89f86ab2a290fe8cf7a2c1033
DavidMikeSimon <david.mike.simon@gmail.com>
2009-09-28 01:35:16 +0000

    Ported (most of) the python geometry module

---
+++ cpp/geometry.cpp
@@ -0,0 +1,147 @@
+#include <GL/gl.h>
+
+#include <cmath>
+#include <string>
+#include <sstream>
+
+#include "geometry.h"
+
+const GLfloat near_to_delta = 0.0001;
+
+GLfloat rev2rad(GLfloat ang) {
+	return 2.0*ang*M_PI;
+}
+
+GLfloat rad2rev(GLfloat ang) {
+	return ang/(M_PI*2.0);
+}
+
+GLfloat rev2deg(GLfloat ang) {
+	return 360.0*ang;
+}
+
+GLfloat deg2rev(GLfloat ang) {
+	return ang/360.0;
+}
+
+GLfloat rad2deg(GLfloat ang) {
+	return (360.0*ang)/(M_PI*2);
+}
+
+GLfloat deg2rad(GLfloat ang) {
+	return (ang/360.0)*2*M_PI;
+}
+
+Point& Point::operator=(const Point& other) {
+	x = other.x;
+	y = other.y;
+	z = other.z;
+	return *this;
+}
+
+std::string Point::to_str() const {
+	// FIXME : Implement
+}
+
+Point Point::operator+(const Point& other) const {
+	return Point(x + other.x, y + other.y, z + other.z);
+}
+
+void Point::operator+=(const Point& other) {
+	x += other.x;
+	y += other.y;
+	z += other.z;
+}
+
+Point Point::operator-(const Point& other) const {
+	return Point(x - other.x, y - other.y, z - other.z);
+}
+
+void Point::operator-=(const Point& other) {
+	x -= other.x;
+	y -= other.y;
+	z -= other.z;
+}
+
+Point Point::operator*(const Point& other) const {
+	return Point(x*other.x, y*other.y, z*other.z);
+}
+
+Point Point::operator*(GLfloat f) const {
+	return Point(x*f, y*f, z*f);
+}
+
+void Point::operator*=(const Point& other) {
+	x *= other.x;
+	y *= other.y;
+	z *= other.z;
+}
+
+void Point::operator*=(GLfloat f) {
+	x *= f;
+	y *= f;
+	z *= f;
+}
+
+Point Point::operator/(const Point& other) const {
+	return Point(x/other.x, y/other.y, z/other.z);
+}
+
+Point Point::operator/(GLfloat f) const {
+	return Point(x/f, y/f, z/f);
+}
+
+void Point::operator/=(const Point& other) {
+	x /= other.x;
+	y /= other.y;
+	z /= other.z;
+}
+
+void Point::operator/=(GLfloat f) {
+	x /= f;
+	y /= f;
+	z /= f;
+}
+
+/* FIXME
+bool operator==(const Point& other) const {
+	return x == other.x && y == other.y && z == other.z;
+}
+*/
+
+Point Point::operator-() const {
+	return Point(-x, -y, -z);
+}
+
+bool Point::near_to(const Point& other) const {
+	return (
+		std::abs(x - other.x) < near_to_delta &&
+		std::abs(y - other.y) < near_to_delta &&
+		std::abs(z - other.z) < near_to_delta
+	);
+}
+
+GLfloat Point::mag() const {
+	return std::sqrt(pow(std::sqrt(x*x + y*y), 2) + z*z);
+}
+
+GLfloat Point::dot_prod(const Point& other) const {
+	return x*other.x + y*other.y + z*other.z;
+}
+
+GLfloat Point::dist_to(const Point& other) const {
+	return std::sqrt(pow(std::sqrt(pow(x-other.x, 2) + pow(y-other.y, 2)), 2) + pow(z-other.z, 2));
+}
+
+GLfloat Point::ang_to(const Point& other) const {
+	return rad2rev(std::acos((dot_prod(other))/(mag()*other.mag())));
+}
+
+Point Point::to_length(GLfloat len) const {
+	GLfloat old_len = mag();
+	if (old_len != 0.0) {
+		return *this/old_len;
+	} else {
+		return *this;
+	}
+}
---
+++ cpp/geometry.h
@@ -0,0 +1,58 @@
+#ifndef ORBIT_RIBBON_GEOMETRY_H
+#define ORBIT_RIBBON_GEOMETRY_H
+
+#include <GL/gl.h>
+
+#include <string>
+
+GLfloat rev2rad(GLfloat ang); // FIXME: In old version, this also converted cw to ccw. Make sure everything that needed that does it itself!
+GLfloat rad2rev(GLfloat ang);
+GLfloat rev2deg(GLfloat ang);
+GLfloat deg2rev(GLfloat ang);
+GLfloat rad2deg(GLfloat ang);
+GLfloat deg2rad(GLfloat ang);
+
+class Point {
+	public:
+		GLfloat x;
+		GLfloat y;
+		GLfloat z;
+		
+		Point(GLfloat nx = 0.0, GLfloat ny = 0.0, GLfloat nz = 0.0) : x(nx), y(ny), z(nz) {}
+		Point(const Point& other) : x(other.x), y(other.y), z(other.z) {}
+		
+		Point& operator=(const Point& other);
+		
+		Point operator+(const Point& other) const;
+		void operator+=(const Point& other);
+		
+		Point operator-(const Point& other) const;
+		void operator-=(const Point& other);
+		
+		Point operator*(const Point& other) const;
+		Point operator*(GLfloat f) const;
+		void operator*=(const Point& other);
+		void operator*=(GLfloat f);
+		
+		Point operator/(const Point& other) const;
+		Point operator/(GLfloat f) const;
+		void operator/=(const Point& other);
+		void operator/=(GLfloat f);
+		
+		// FIXME
+		//bool operator==(const Point& other) const;
+		//bool operator!=(const Point& other) const { return !(*this == other); }
+		
+		Point operator-() const;
+		
+		bool near_to(const Point& other) const;
+		GLfloat dot_prod(const Point& other) const;
+		GLfloat mag() const;
+		GLfloat dist_to(const Point& other) const;
+		GLfloat ang_to(const Point& other) const;
+		Point to_length(GLfloat len) const;
+		
+		std::string to_str() const;
+};
+
+#endif
