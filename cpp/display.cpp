e661f19cc69d936da6754326a573f8e71de21059
DavidMikeSimon <david.mike.simon@gmail.com>
2010-01-31 07:30:22 +0000

    Enabled back face culling

--- cpp/display.cpp
+++ cpp/display.cpp
@@ -171,6 +171,7 @@ void Display::init() {
 	glDepthFunc(GL_LEQUAL);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_CULL_FACE);
 	
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glEnableClientState(GL_NORMAL_ARRAY);
