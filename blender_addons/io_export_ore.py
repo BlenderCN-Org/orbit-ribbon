e28f69e4a95764eb6665526a03911c6401fee19c
David Mike Simon <david.mike.simon@gmail.com>
2011-02-27 15:19:43 -0500

    Ported the first few parts of orbit-edit.py export

--- blender_addons/io_export_ore.py
+++ blender_addons/io_export_ore.py
@@ -27,10 +27,35 @@ bl_addon_info = {
   "category": "Import-Export"
 }
 
-import bpy
+import bpy, mathutils
 from io_utils import ExportHelper
-import os, re, sys, zipfile, datetime
+import os, re, sys, zipfile, datetime, xml.dom.minidom
 from math import *
+
+MATRIX_BLEN2ORE = mathutils.Matrix.Rotation(-90, 4, 'X')
+MATRIX_INV_BLEN2ORE = MATRIX_BLEN2ORE.copy().invert()
+
+OREANIM_NAMESPACE = "http://www.orbit-ribbon.org/OREAnim1"
+OREANIM_NS_PREFIX = "{%s}" % OREANIM_NAMESPACE
+OREANIM_NSMAP = {"oreanim" : OREANIM_NAMESPACE}
+
+def fixcoords(t): # Given a 3-sequence, returns it so that axes changed to fit OpenGL standards (y is up, z is forward)
+  t = mathutils.Vector(t) * MATRIX_BLEN2ORE
+  return (t[0], t[1], t[2])
+
+def genrotmatrix(x, y, z): # Returns a 9-tuple for a column-major 3x3 rotation matrix with axes corrected ala fixcoords
+  m = (
+    MATRIX_INV_BLEN2ORE *
+    mathutils.Matrix.Rotation(radians(x), 4, 'X') *
+    mathutils.Matrix.Rotation(radians(y), 4, 'Y') *
+    mathutils.Matrix.Rotation(radians(z), 4, 'Z') *
+    MATRIX_BLEN2ORE
+  )
+  return ( # Elide final column and row
+    m[0][0], m[0][1], m[0][2],
+    m[1][0], m[1][1], m[1][2],
+    m[2][0], m[2][1], m[2][2],
+  )
 
 
 class Export_Ore(bpy.types.Operator, ExportHelper):
@@ -45,7 +70,22 @@ class Export_Ore(bpy.types.Operator, ExportHelper):
     return True
   
   def execute(self, context):
-    print("Writing ORE %s" % self.filepath)
+    # Load the description from the oredesc text datablock
+    descDoc = None
+    if not "oredesc" in bpy.data.texts.keys():
+      self.report({'ERROR'}, "Couldn't find the oredesc text")
+      return {'CANCELLED'}
+    try:
+      descDoc = xml.dom.minidom.parseString(bpy.data.texts["oredesc"].as_string())
+    except Exception as e:
+      self.report({'ERROR'}, "Parsing error: %s" % e)
+      return {'CANCELLED'}
+
+    # Create a new ORE file; just a regular zipfile
+    zfh = zipfile.ZipFile(self.filepath, mode = "w", compression = zipfile.ZIP_DEFLATED)
+    zfh.writestr("ore-version", "1") # ORE file format version (this will be 1 until the first backwards-incompatible change after release)
+    zfh.writestr("ore-name", descDoc.xpath("niceName/text()")[0]) # The nice name of the ORE package, separate so that we can get it without XML
+
     return {'FINISHED'}
   
   def invoke(self, context, event):
